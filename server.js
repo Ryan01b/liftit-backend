cf19e33fd20e1bc621bfd5168dffeba820f154be
Rayene Belaid <89009953+Ryan01b@users.noreply.github.com>
2026-05-05 16:14:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.use('/api/exercises', exerciseRoutes);
 
 // Test route
 app.get('/', (req, res) => {
-  res.send('✅ LiftIt Backend is running! 💪');
+  res.send('LiftIt Backend is running! ');
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(` Server is running on http://localhost:${PORT}`);
 });
