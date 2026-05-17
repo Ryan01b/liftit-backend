f4be79ad5e215736302b7c442193efa337e9e0e1
Rayene Belaid <ryanbelaid2006@gmail.com>
2026-05-17 22:37:14 +0200

    Add React frontend and update assets

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header className="header">
-      <h1>💪 LiftIt</h1>
+      <h1> LiftIt</h1>
       <p>Workout & Exercise Tracker</p>
     </header>
   );
